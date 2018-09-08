b2ea226a8b1ca1a57d67011bb0759a6b90777a4e
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 01:45:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(array)
     position = array.index(name)
     final_position = position + 1
     name_and_position = "#{final_position}. #{name}"
-    name_of_people_in_line.push(name_and_position)
+    name_of_people_in_line.push(name_and_position.to_s)
     end
     puts "The line is currently: #{name_of_people_in_line}"
   end 
