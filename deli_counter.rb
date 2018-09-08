53d29462a51926f447460c12af14b4809ca49e03
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 01:32:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,17 @@
 # Write your code here.
 
 def line(array)
-  if array.count == 0 
-    "The line is currently empty."
+  if array.count < 1 
+    puts "The line is currently empty."
   else
     for i in array
     name = array[i]
     position = i + 1
-    name_and_position = "#{position}. #{name}"
+    name_and_position = "#{position.to_s}. #{name}"
     name_of_people_in_line = []
     name_of_people_in_line.push(name_and_position)
     end
-    name_of_people_in_line
+    puts "The line is currently: #{name_of_people_in_line}"
   end 
 end
 
